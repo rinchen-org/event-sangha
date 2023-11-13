faf751bb9d74f39af45231e78204b75833c4ab33
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2023-11-13 11:37:48 -0400

    fix: fix global variable

--- src/attendance_log.php
+++ src/attendance_log.php
@@ -2,11 +2,11 @@
 
 require_once "./lib/settings.php";
 
-global $URL_BASE;
+global $BASE_URL;
 
 // Redirect to templates/index.php
 $params = str_replace("+", "%2B", $_SERVER['QUERY_STRING']);
-$url = "{$URL_BASE}/templates/attendance/log.php?{$params}";
+$url = "{$BASE_URL}/templates/attendance/log.php?{$params}";
 
 header("Location: {$url}");
 exit; // Make sure to exit after the redirection

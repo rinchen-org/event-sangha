ba5728651b2223e507a697d84d856b6dd9f25e52
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2023-11-24 18:20:26 -0400

    improve options for the datatable for attendance

--- src/templates/attendance/list.php
+++ src/templates/attendance/list.php
@@ -47,8 +47,26 @@ if ($result): ?>
 
 <a href="../" class="btn btn-warning my-3">Back to the menu</a>
 
+<script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+<script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.print.min.js"></script>
+
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.dataTables.min.css">
+
 <script>
-    let table = new DataTable('#attendance_list');
+    // let table = new DataTable('#attendance_list');
+    $(document).ready(function() {
+      $('#attendance_list').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [ 'copy', 'csv', 'excel', 'pdf', 'print', 'pageLength' ]
+      });
+    });
 </script>
 
 
